dffc23181fb24702b6263241b611f83e67fcdd76
Guillaume APCHAIN <guillaume.apchain@gmail.com>
2024-08-12 17:59:36 +0200

    Add files via upload

---
+++ packageLocally.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+rm -rf ./secr
+mkdir ./secr
+if [ "$VIRTUAL_ENV" == "" ]
+then
+    echo "No venv found, we activate it !"
+    source $HOME/.pyenv/versions/venv_quart/bin/activate
+else
+    echo "(venv) already activated, just fine !"
+fi
+
+# sudo rm -fr dist build *egg*
+echo y | pip uninstall my-quart-api
+$VIRTUAL_ENV/bin/python -m pip install --upgrade pip
+pip install -r requirements.txt
+sed -i "s/\"__VERSION__\"/\"1.0.0\"/g" ./build.py
+pyb
+WHL=$(find -type f -name "*.whl")
+pip install ${WHL}  --force-reinstall
+sed -i "s/\"1.0.0\"/\"__VERSION__\"/g" ./build.py
+
+{
+  echo "ENV=dev"
+  echo "PYTHONUNBUFFERED=1"
+  echo "SERVER_PORT=10002"
+} >> secr/.env
+
+
+
+
